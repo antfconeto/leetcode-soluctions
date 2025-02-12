c5c5dfb214aa825deb4ec99da3f2479a09a3124c
antfconeto <antfcone>
2025-02-12 13:49:39 -0300

    feat: ✨ resolved the problem 4 of leetcode

---
+++ hard/4.ts
@@ -0,0 +1,12 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]) {
+    let newArray:number[] = []
+    nums1.forEach((num)=>newArray.push(num))
+    nums2.forEach((num)=>newArray.push(num))
+    let a = newArray.sort((a,b)=>(a-b))
+    if(newArray.length % 2 != 0){
+        return a[(a.length + 1)/2-1]
+    }
+    return (a[(a.length/2)] + a[(a.length/2 - 1)])/2
+};
+
+console.log(findMedianSortedArrays([1,2,3,4,5],[6,7,8,9,10,11,12,13,14,15,16,17]))
