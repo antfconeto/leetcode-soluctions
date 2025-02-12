53eb93daa33f41d45e2445fd9d8ed543c5cb4b11
antfconeto <antfcone>
2025-02-12 13:23:59 -0300

    feat: :sparkles: resolved the problem 3 of leetcode

---
+++ medium/3.ts
@@ -0,0 +1,19 @@
+function lengthOfLongestSubstring(s: string): number {
+    let map = new Map<string, number>();
+    let maxLength = 0;
+    let left = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        
+        if (map.has(currentChar)) {
+            left = Math.max(map.get(currentChar)! + 1, left);
+        }
+
+        map.set(currentChar, right);
+
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
